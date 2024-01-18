aa4b10026c1e77bdade922a6f0216dda0899c08b
Sachin Sudani <sachinsudani60@gmail.com>
2024-01-18 11:51:04 +0530

    learn: arrow function

---
+++ javascript-basics/ex-2.js
@@ -0,0 +1,28 @@
+const name = 'John';
+let age = 25;
+const hasHobbies = true;
+
+age = 30;
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+	return (
+		'Name is ' +
+		userName +
+		', age is ' +
+		userAge +
+		' and the user has hobbies: ' +
+		userHasHobby
+	);
+};
+
+const add = (a, b) => a + b;
+const addOne = (a) => a + 1;
+const addRandom = () => 1 + 2;
+
+console.log(summarizeUser(name, age, hasHobbies));
+
+console.log(add(1, 2));
+
+console.log(addOne(1));
+
+console.log(addRandom());
