27e1f5dadee388023f6896ea5cff51dc7b91a4a9
Sachin Sudani <sachinsudani60@gmail.com>
2024-01-27 07:55:22 +0530

    Add custom 404 page

--- express-js/app.js
+++ express-js/app.js
@@ -2,7 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import adminRoutes from './routes/admin.js';
 import shopRoutes from './routes/shop.js';
+import path from 'path';
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +13,9 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).send('<h1>Page not found</h1>');
+	res
+		.status(404)
+		.sendFile(path.join(__dirname, 'express-js', 'views', '404.html'));
 });
 
 console.log('Server is running on port 3000');
